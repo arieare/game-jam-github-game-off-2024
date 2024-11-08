1f1df1d614a32e49e36091eff0bd0c47a5be36f8
Arie Aulia Nugraha <arie.a.re@gmail.com>
2024-11-08 22:38:45 +0700

    fixing issue with dictionary load especially for web export

--- app/_root.gd
+++ app/_root.gd
@@ -9,15 +9,24 @@ var data_instance: Node
 @export var root_game_select_menu: PackedScene
 
 
+@onready var icon = load("res://app/app-icon.png")
+@onready var data:PackedScene = load("res://collection/rogue_knight/game/core/game_data.tscn")
+@export var game_node: Dictionary[String, PackedScene]
+@onready var game = {"game_node":game_node}
+
+@export var ui_node: Dictionary[String, PackedScene]
+@onready var ui = {"ui_node":ui_node}
+
 @onready var game_instance: Dictionary = {
 	"rogue_knight": {
 		"name": "Rogue Knight",
-		"icon":  ResourceLoader.load("res://app/app-icon.png"),
-		"data": preload("res://collection/rogue_knight/game/core/game_data.tscn"),
-		"game": preload("res://collection/rogue_knight/game/core/game_rogue_knight.tres"),
-		"ui": preload("res://collection/rogue_knight/game/core/ui_rogue_knight.tres")
+		"icon": icon,
+		"data": data,
+		"game": game,
+		"ui": ui
 		},				
 }
 func _ready() -> void:
+	print(ui)
 	util.emit_signal("root_ready", self)
 	ui_container.add_child(root_menu.instantiate())
--- app/root_main_menu/btn_start.gd
+++ app/root_main_menu/btn_start.gd
@@ -1,16 +1,40 @@
-extends ui_btn_change_scene
+extends Button
+
+var game_node
+var ui_node
+var data_node
 
 func _ready():
+	await get_tree().root.ready
 	game_node = null
 	ui_node = util.root.game_instance["rogue_knight"].ui.ui_node.main_menu
-	#data_node = util.root.game_instance["rogue_knight"].data
+	data_node = util.root.game_instance["rogue_knight"].data
+	print(ui_node)
+	print(data_node)
 	#util.root.data_instance = util.root.data_container.get_child(0)
 
 func _input(event: InputEvent) -> void:
 	if !event.is_echo() and self.button_pressed:
-		print("hello")
-		#util.scene_manager.change( util.root.ui_container, util.root.game_instance[key].ui.ui_node.main_menu)
-		#print(util.root.game_instance.data[key])
-		util.scene_manager.change( util.root.data_container, util.root.game_instance["rogue_knight"].data)
-		util.root.data_instance = util.root.data_container.get_child(0)
+		game_node = null
+		#ui_node = util.root.game_instance["rogue_knight"].ui.ui_node.main_menu
+		#data_node = util.root.game_instance["rogue_knight"].data		
+		#print("hello")
+		##util.scene_manager.change( util.root.ui_container, util.root.game_instance[key].ui.ui_node.main_menu)
+		##print(util.root.game_instance.data[key])
+		#util.scene_manager.change( util.root.data_container, util.root.game_instance["rogue_knight"].data)
+		#util.root.data_instance = util.root.data_container.get_child(0)
 		scene_update()
+
+
+	
+func scene_update():
+	util.scene_manager.change( util.root.game_container, game_node)	
+	util.scene_manager.change( util.root.ui_container, ui_node)
+	var data = util.root.game_instance["rogue_knight"].data.instantiate()
+	util.root.data_container.add_child(data)
+	util.root.data_instance = util.root.data_container.get_child(0)
+	#util.scene_manager.change( util.root.data_container, data_node)
+	#pass
+
+
+#util.scene_manager.change( util.root.game_container, util.root.game_instance.game.very_leafy_place.node)	
